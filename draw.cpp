d5c92ce69deefacfc9ddf04c2a04953a57e53693
zzy <ziyiz.mail@qq.com>
2025-09-19 19:07:06 +0800

    enable select and merge balls

--- draw.cpp
+++ draw.cpp
@@ -129,10 +129,12 @@ enum EntityType {
 struct Entity {
     EntityType type;
     const char* text;
+    bool active; // 用来控制是否显示这个实体
 
     // ball
     float radius;
     Draggable drag;
+    bool selected; // 当前球是否被选中
 
     // button
     rect r;
@@ -149,7 +151,7 @@ Draggable make_draggable(float x, float y) {
 
 const int WIDTH = 1200;
 const int HEIGHT = 700;
-GameScreen current_screen = SCREEN_Home;
+GameScreen current_screen = SCREEN_Compose;
 bool mouse_was_down = false;
 vec2 mouse_pos;
 bool mouse_pressed;
@@ -168,6 +170,7 @@ void add_button_entity(rect r, rgba32 color, const char* text) {
     entity_count += 1;
 
     e->type = ENTITY_BUTTON;
+    e->active = true;
     e->r = r;
     e->color = color;
     e->text = text;
@@ -183,7 +186,9 @@ void add_ball_entity(float x, float y, float r, const char* text) {
     entity_count += 1;
 
     e->type = ENTITY_BALL;
+    e->active = true;
     e->radius = r;
+    e->selected = false;
     e->drag = make_draggable(x, y);
     e->text = text;
 }
@@ -205,7 +210,12 @@ bool draw_button(rect r, rgba32 color, const char* text) {
     return false;
 }
 
-void draggable_ball(Draggable* d, float r, const char* text) {
+void draw_ball(vec2 pos, float r, const char* text, rgba32 color) {
+    draw_circle(pos.x, pos.y, r, color);
+    draw_text(text, pos.x, pos.y, r*2, Color(1, 1, 1), button_font);
+}
+
+void draggable_ball(Draggable* d, float r, const char* text, rgba32 color) {
     if (!d->dragged && point_in_circle(mouse_pos, d->pos, r) && mouse_pressed) {
         d->dragged = true;
         d->mouse_off = mouse_pos - d->pos;
@@ -216,8 +226,7 @@ void draggable_ball(Draggable* d, float r, const char* text) {
             d->dragged = false;
         }
     }
-    draw_circle(d->pos.x, d->pos.y, r, Color(1, 0, 0));
-    draw_text(text, d->pos.x, d->pos.y, r*2, Color(1, 1, 1), button_font);
+    draw_ball(d->pos, r, text, color);
 }
 
 void on_load() {
@@ -246,7 +255,7 @@ void draw(float dt, int window_width, int window_height, game_inputs inputs) {
         if (draw_button(button1, Color(1, 0.2, 0.3), "知识探索")) {
             current_screen = SCREEN_Explore;
         }
-        
+
         rect button2 = RectWithPosAndSize(WIDTH * 0.6, HEIGHT / 2.0 - button_height * 0.5, button_width, button_height);
         if (draw_button(button2, Color(.2, 0.8, 0.3), "合成探索")) {
             current_screen = SCREEN_Compose;
@@ -260,51 +269,84 @@ void draw(float dt, int window_width, int window_height, game_inputs inputs) {
     } else if (current_screen == SCREEN_Explore) {
         println("探索 !!!!");
 
-        static float x = 100;
-        static float y = 100;
-        static float offx;
-        static float offy;
-        static bool dragged = false;
-
-        rect explain_button = RectWithPosAndSize(x, y, 50, 50);
-
-        if (!dragged && point_in_rect(mouse_pos, explain_button) && mouse_pressed) {
-            dragged = true;
-            offx = mouse_pos.x - x;
-            offy = mouse_pos.y - y;
-        }
-
-        if (dragged) {
-            x = mouse_pos.x - offx;
-            y = mouse_pos.y - offy;
-
-            if (mouse_released) {
-                dragged = false;
-            }
-        }
-
-        static bool explain_opened = false;
-        if (draw_button(explain_button, Color(.2, .3, .6), explain_opened ? "关闭" : "打开")) {
-            explain_opened = !explain_opened;
-        }
-       
     } else if (current_screen == SCREEN_Compose) {
         println("合成 !!!!");
-        // if (mouse_pressed) {
-        // }
 
-        if (draw_button(RectWithPosAndSize(100, 100, 200, 200), Color(0.2, 0.1, 0.7), "New")) {
-            add_ball_entity(100, 100, 50, "abc");
+        static bool is_selecting = false; // 使用这个变量来控制现在是否在选择球
+
+        // 根据状态在左上角显示不同的文字
+        if (is_selecting) {
+            println("点击球以选择");
+            println("三个abc球可以合成一个D球");
+        } else {
+            println("按住球可拖动");
+            println("点击合成按钮可以选择球进行合成");
         }
 
-        if (draw_button(RectWithPosAndSize(500, 100, 300, 200), Color(0.2, 0.1, 0.7), "delete")) {
+        // 画一个可以拖出新球的球
+        float x = 100;
+        float y = 400;
+        float r = 20;
+        draw_circle(x, y, r, Color(1, 0, 0));
+        if (point_in_circle(mouse_pos, v2(x, y), r) && mouse_pressed) {
+            add_ball_entity(x, y, r, "abc"); // 添加一个abc球
+        }
+
+        // 画删除按钮
+        if (draw_button(RectWithPosAndSize(500, 100, 200, 100), Color(0.2, 0.1, 0.7), "delete")) {
             entity_count = 0;
         }
 
+        // 画用来控制是否合成的按钮
+        rect selecting_button = RectWithPosAndSize(200, 400, 100, 50);
+        rgba32 selecting_button_color = Color(0.2, 0.6, 0.3);
+        const char* selecting_button_text = is_selecting ?  "取消合成" : "合成";
+        // 按钮按下时：
+        if (draw_button(selecting_button, selecting_button_color, selecting_button_text)) {
+            is_selecting = !is_selecting; // 切换合成状态
+        }
+
+        Entity* selected_balls[10]; // 使用这个数组记录当前被选择的球
+        int selected_ball_count = 0; // 当前选择了的球数量
+
+        // 处理所有的实体
         for (int i = 0; i < entity_count; ++i) {
             Entity* e = &entities[i];
+            if (!e->active) continue; // 跳过非active的实体
+
             if (e->type == ENTITY_BALL) {
-                draggable_ball(&e->drag, 20, "abc");
+                rgba32 ball_color = Color(1, 0, 0);
+                if (is_selecting) {
+                    // 当在选择时：
+                    if (point_in_circle(mouse_pos, e->drag.pos, e->radius)) {
+                        // 鼠标按下时设置它的selected状态
+                        if (mouse_pressed) {
+                            e->selected = !e->selected;
+                        }
+                    }
+                    if (e->selected) {
+                        // 如果当前被选中
+                        ball_color = Color(0.8, 0.6, 0.2); // 改变颜色
+                        selected_balls[selected_ball_count] = e;
+                        selected_ball_count += 1;
+
+                        if (selected_ball_count == 3) {
+                            // 如果选中了3个球
+                            for (int i = 0; i < selected_ball_count; ++i) {
+                                selected_balls[i]->active = false; // 删除被选中的球
+                            }
+                            selected_ball_count = 0;
+                            add_ball_entity(e->drag.pos.x, e->drag.pos.y, e->radius, "D"); // 在当前球的位置生成一个D球
+                        }
+                    }
+
+                    // 画球
+                    draw_ball(e->drag.pos, e->radius, e->text, ball_color);
+                } else {
+                    // 如果不在选择：
+                    e->selected = false; // 清除球的选择状态
+                    draggable_ball(&e->drag, e->radius, e->text, ball_color); // 显示拖动球
+                }
             } else if (e->type == ENTITY_BUTTON) {
                 draw_button(e->r, e->color, e->text);
             }
